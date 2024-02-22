32a9c96a5acf2be7ff7d71c05fd9cfd490b31db8
Sadinus <pindelski.pawel@gmail.com>
2024-02-23 00:38:13 +0100

    Styled car list

--- src/app/cars/CarList.tsx
+++ src/app/cars/CarList.tsx
@@ -16,23 +16,32 @@ const CarList = ({ cars, carType }: Props) => {
   return (
     <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {filteredCars.map((car) => (
-        <li key={car.id}>
+        <li
+          key={car.id}
+          className="relative max-w-sm rounded overflow-hidden shadow-lg"
+        >
           <Image
             src={"/carimage.jpg"}
-            width={"200"}
-            height={"200"}
+            width={1178}
+            height={762}
             alt={car.title}
             className="w-full h-40 object-cover mb-2"
           />
-          <h2 className="text-xl font-semibold mb-1">{car.title}</h2>
-          <p className="text-gray-600 mb-2">{car.brand}</p>
-          <p className="text-gray-600">{car.vehicleType}</p>
-          <Link
-            href={car.link.href}
-            className="text-blue-600 font-bold mt-2 block"
-          >
-            View Details
-          </Link>
+          <div className="px-6 py-6 pb-8">
+            <div className="font-bold text-xl mb-2">{car.brand}</div>
+            <p className="text-gray-700 text-base mb-2">{car.title}</p>
+            <p className="text-gray-700 text-base mb-2">
+              carType: {car.vehicleType}
+            </p>
+            <Link
+              href={car.link.href}
+              className="absolute bottom-0 right-0 mb-4 mr-4 mt-8"
+            >
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                View Details
+              </button>
+            </Link>
+          </div>
         </li>
       ))}
     </ul>
