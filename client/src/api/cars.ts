f81a82eeccd35e429c65db8f3c91d3133312f43f
Sadinus <pindelski.pawel@gmail.com>
2024-02-29 17:50:07 +0100

    configured netlify

--- client/src/api/cars.ts
+++ client/src/api/cars.ts
@@ -1,15 +1,26 @@
 import { Car } from "src/app/types";
+import data from "./data.json";
+
+// data.json used only to show the demo on netlify
 
 export async function getCars() {
-  return fetch(`${process.env.API_URL}/cars`)
-    .then((res) => res.json())
-    .then((data) => data as Car[]);
+  if (process.env.NODE_ENV === "production") {
+    return data.cars as Car[];
+  } else {
+    return fetch(`${process.env.API_URL}/cars`)
+      .then((res) => res.json())
+      .then((data) => data as Car[]);
+  }
 }
 
 export async function getCar(id: string) {
-  const carHref = `/cars/${id}`;
+  if (process.env.NODE_ENV === "production") {
+    return data.cars.find((x) => x.link.href === `/cars/${id}`) as Car;
+  } else {
+    const carHref = `/cars/${id}`;
 
-  return fetch(`${process.env.API_URL}/cars?link.href=${carHref}`)
-    .then((res) => res.json())
-    .then((data) => data[0] as Car);
+    return fetch(`${process.env.API_URL}/cars?link.href=${carHref}`)
+      .then((res) => res.json())
+      .then((data) => data[0] as Car);
+  }
 }
