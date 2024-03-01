fb54f6d1d60b1f4e49069cfcd36679772cf50dd5
Sadinus <pindelski.pawel@gmail.com>
2024-03-01 02:24:38 +0100

    Car loading styling

--- client/src/app/cars/[id]/loading.tsx
+++ client/src/app/cars/[id]/loading.tsx
@@ -2,7 +2,7 @@ import { SkeletonLine } from "../components/Skeleton";
 
 export default function Loading() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto px-6 md:px-2">
       <h1 className="text-3xl font-bold my-4">Car Detail</h1>
       <SkeletonLine />
     </div>
--- client/src/app/cars/components/Skeleton.tsx
+++ client/src/app/cars/components/Skeleton.tsx
@@ -30,7 +30,7 @@ export const SkeletonCard = () => {
 export function SkeletonLine() {
   return (
     <>
-      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-2.5" />
+      <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-60 mb-2.5" />
       <span className="sr-only">Loading...</span>
     </>
   );
