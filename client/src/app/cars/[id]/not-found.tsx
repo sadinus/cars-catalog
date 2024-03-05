254ef43bb0ba76683cf8008c35cafec53c1889db
Sadinus <pindelski.pawel@gmail.com>
2024-03-05 12:50:40 +0100

    Styled not-found page

--- client/src/app/cars/[id]/not-found.tsx
+++ client/src/app/cars/[id]/not-found.tsx
@@ -4,9 +4,16 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <>
-      <h2>Car with this id does not exist</h2>
-      <Link href={".."}>Go back to list of cars</Link>
-    </>
+    <div className="container mx-auto px-6 md:px-2">
+      <h2 className="text-3xl font-bold my-4">
+        Car with this id does not exist
+      </h2>
+      <Link
+        className={"text-xl text-blue-600 dark:text-blue-500 hover:underline"}
+        href={".."}
+      >
+        Go back to list of cars
+      </Link>
+    </div>
   );
 }
