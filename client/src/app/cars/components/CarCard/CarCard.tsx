4e546bf8b331a1a5068584826c83769741118b04
Sadinus <pindelski.pawel@gmail.com>
2024-03-08 12:30:24 +0100

    Added sizes prop to Image

--- client/src/app/cars/components/CarCard/CarCard.tsx
+++ client/src/app/cars/components/CarCard/CarCard.tsx
@@ -14,6 +14,7 @@ export const CarCard = ({ car }: Props) => {
         <Image
           src={carImage}
           fill
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           placeholder="blur"
           priority
           alt={car.title}
